73525fda070b8fd27bfbf2de9b433dead0c42e76
shkmd <sm2877@gmail.com>
2020-01-22 18:18:19 +0400

    Tempates

--- assets/js/pages/forms/editors.js
+++ assets/js/pages/forms/editors.js
@@ -2,5 +2,6 @@ $(function () {
     //CKEditor
     CKEDITOR.replace('ckeditor');
     CKEDITOR.config.height = 300;
-  
+    //CKEDITOR.config.removeButtons = 'Source';
+  	CKEDITOR.config.removeButtons = 'Underline,Subscript,Superscript';
 });
