ff15699c465d0896e52c12806c75968f598c3224
shkmd <sm2877@gmail.com>
2020-02-03 09:53:45 +0400

    Preview.html Added toggle switcher

---
+++ assets/js/custom.js
@@ -0,0 +1,17 @@
+    $(document).ready(function() {
+    $('#customSwitches').change(function() {
+        $('#mycheckboxdiv').toggle();
+    });
+    $('#customSwitches2').change(function() {
+        $('#mycheckboxdiv2').toggle();
+    });
+    $('#customSwitches3').change(function() {
+        $('#mycheckboxdiv3').toggle();
+    });
+    $('#customSwitches4').change(function() {
+        $('#mycheckboxdiv4').toggle();
+    });
+    $('#customSwitches5').change(function() {
+        $('#mycheckboxdiv5').toggle();
+    });
+});
--- assets/vendor/tinymce/tinymce-script.js
+++ assets/vendor/tinymce/tinymce-script.js
@@ -1,5 +1,20 @@
 tinymce.init({
   selector: 'textarea#default',  // change this value according to your HTML
-  height: 400,
-  statusbar: false
+  height: 300,
+  statusbar: false,
+  menubar: false,
+
+  formats: {
+    removeformat: [
+      // Configures `clear formatting` to remove specified elements regardless of it's attributes
+      { selector: 'b,strong,em,i,font,u,strike', remove: 'all' },
+
+      // Configures `clear formatting` to remove the class red from spans and if the element then becomes empty i.e has no attributes it gets removed
+      { selector: 'span', classes: 'red', remove: 'empty' },
+
+      // Configures `clear formatting` to remove the class green from spans and if the element then becomes empty it's left intact
+      { selector: 'span', classes: 'green', remove: 'none' }
+    ]
+  }
+
 });
